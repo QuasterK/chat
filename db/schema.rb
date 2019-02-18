66259de285dc15a3d5b057b3049bd465f1c0a828
Piotr Komorowski <komorowski.piotr.91@gmail.com>
2019-02-18 20:15:38 +0100

    added has_secure_password

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_02_18_185941) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "nick"
+    t.string "name"
+    t.string "surname"
+    t.string "password_digest"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
