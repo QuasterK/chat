1296aa5c6ca8efc0d088c69d935fa48113a01623
Piotr Komorowski <komorowski.piotr.91@gmail.com>
2019-02-19 22:12:34 +0100

    created add messages input

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -3,4 +3,11 @@ class ChatroomController < ApplicationController
   def index
     @messages = Message.all
   end
+
+  def create
+    message = Message.new(data: params[:data], user_id: current_user.id)
+    message.save
+    redirect_to root_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'chatroom#index'
+  post 'create_message', to: 'chatroom#create'
   resources :users
   get 'signup', to: 'users#new', as: 'signup'
   resources :session, only: [:new, :create, :destroy]
