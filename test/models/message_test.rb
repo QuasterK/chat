4338bc1b182d14c049b53c4d7b48cebf2c542aa4
Piotr Komorowski <komorowski.piotr.91@gmail.com>
2019-02-20 17:03:38 +0100

    added models unit tests

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "should have data" do
+    message = Message.new
+    assert_not message.save
+  end
+
+  test "should belong to user" do
+    message = Message.new(data: 'some dummy data')
+    assert_not message.save
+  end
+
+  test "should save message with data and user id" do
+    user = User.first
+    message = Message.new(data: 'some dummy data', user_id: user.id)
+    assert message.save
+  end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(email: 'email@email.pl', nick: 'nick')
+  end
+
+  test "should not save user without email" do
+    @user.email = ''
+    assert_not @user.save
+  end
+
+  test "should not save user without nick" do
+    @user.nick = ''
+    assert_not @user.save
+  end
+
+  test "should not save user if nick isn't uniq" do
+    @user.save
+    user2 = User.new(email: 'email2@email.pl', nick: 'nick')
+    assert_not user2.save
+  end
+
+  test "should not save user if email isn't uniq" do
+    @user.save
+    user2 = User.new(email: 'email@email.pl', nick: 'nick2')
+    assert_not user2.save
+  end
+  # password tests
+  test "should not save user without password" do
+    user = User.new(email: 'uniq@email.pl', nick: 'uniq')
+    assert_not user.save
+  end
+
+  test "should not save user without same value of password and password_confirmation" do
+    user = User.new(email: 'uniq@email.pl', nick: 'uniq', password: '123456', password_confirmation: 'wrong password')
+    assert_not user.save
+  end
+
+  test "should save user with the same value of password and password_confirmation" do
+    user = User.new(email: 'uniq@email.pl', nick: 'uniq', password: '123', password_confirmation: '123')
+    assert user.save
+  end
+
 end
