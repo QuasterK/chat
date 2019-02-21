b6439daee27e112dbe42504e1e8ca6a6fdfb4a04
Piotr Komorowski <komorowski.piotr.91@gmail.com>
2019-02-21 02:19:58 +0100

    push to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.web_socket_server_url = "wss://my-chatty-chat.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://my-chatty-chat.herokuapp.com/cable', 'http://my-chatty-chat.herokuapp.com/cable']
 
   # Code is not reloaded between requests.
   config.cache_classes = true
